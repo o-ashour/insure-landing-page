faa90f945c8631228149d6b4dc4fb33e7010a639
Omar Ashour <omar.a.ashour@gmail.com>
2022-04-06 21:50:56 +0200

    Added interaction to nav menu mobile

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -118,7 +118,23 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"script.js":[function(require,module,exports) {
-console.log('hello');
+// Elements
+var navIcon = document.querySelector('.nav-menu-icon');
+var navMenu = document.querySelector('nav'); // Event listeners
+
+navIcon.addEventListener('click', toggleNav); // open and close nav menu and switch icons
+
+function toggleNav() {
+  navMenu.classList.toggle('nav-open');
+
+  if (navMenu.classList.contains('nav-open')) {
+    navIcon.setAttribute('src', '/icon-close.1add023a.svg');
+    navIcon.setAttribute('alt', 'Close menu icon');
+  } else {
+    navIcon.setAttribute('src', '/icon-hamburger.cb455b0f.svg');
+    navIcon.setAttribute('alt', 'Hamburger menu icon');
+  }
+}
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49357" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49417" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+// Elements
+const navIcon = document.querySelector('.nav-menu-icon');
+const navMenu = document.querySelector('nav');
+
+// Event listeners
+navIcon.addEventListener('click', toggleNav);
+
+// open and close nav menu and switch icons
+function toggleNav(){
+  navMenu.classList.toggle('nav-open');
+  if(navMenu.classList.contains('nav-open')){
+    navIcon.setAttribute('src', '/icon-close.1add023a.svg');
+    navIcon.setAttribute('alt', 'Close menu icon');
+  }
+  else {
+    navIcon.setAttribute('src', '/icon-hamburger.cb455b0f.svg');
+    navIcon.setAttribute('alt', 'Hamburger menu icon');
+  }
+}
